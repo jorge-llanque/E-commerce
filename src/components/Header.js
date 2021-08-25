9c9c989bd322ba54b07feb7155b507b32bfeefb2
jorge <georgellanque@gmail.com>
2021-08-24 22:24:18 -0400

    Add Header and content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,48 @@
 import React from 'react'
 
 export default function Header() {
+
+
   return (
-    <div>
-      Header
-    </div>
+    <header className="Header">
+      <div className="Header__Row1">
+        <p>Descubre los beneficios de ser parte Coppel Max</p>
+        <img src="#" alt="imageads"/>
+      </div>
+      <div className="Header__Row2">
+        <div className="Header__Row2-Sideleft">
+          <img src="#" alt="logo" />
+          <div className="Header__Row2-Search">Search</div>
+        </div>
+        <div className="Header__Row2-Sideright">
+          <nav>
+            <ul>
+              <li>Comprar ahora</li>
+              <li>Enviar a:</li>
+              <li>Inicia sesión</li>
+              <li>Carrito</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className="Header__Row3">
+        <div className="Header__Row3-Sideleft">
+          <span>Departamentos</span>
+          <span>Ofertas</span>
+          <span>Exclusivos en línea</span>
+          <span>Servicios</span>
+        </div>
+        <div className="Header__Row3-Sideright">
+          <span>Ubica tu tienda</span>
+          <span>Sigue tus pedidos</span>
+          <span>Ayuda</span>
+          <span>Theme Color</span>
+        </div>
+        <div className="Hedaer__Row3-Sideright">
+
+        </div>
+      </div>
+    </header>
   )
 }
+
