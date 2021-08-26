8e1263429ce8407a0b57c4cb11d9c0fdb97b525a
jorge <georgellanque@gmail.com>
2021-08-26 18:09:09 -0400

    Add content to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,6 +22,67 @@ export default function Navbar() {
         <a href="/">Conoce cómo abonar</a>
         <a href="/">Préstamo personal</a>
       </section>
+      <section className="Sidebar__Block3">
+        <div>
+          <input type="radio" name="accordion" id="sectionBlock3" className="Accordion__Input"/>
+          <label for="sectionBlock3" className="Accordion__Label">Departamentos</label>
+          <div className="Accordion__Content">
+            <ul>
+              <li>Celulares</li>
+              <li>Electrónica</li>
+              <li>Consolas y Videojuegos</li>
+              <li>Instrumentos Musicales</li>
+              <li>Hogar y Muebles</li>
+              <li>Línea Blanca y Electrodomésticos</li>
+              <li>Zapatos</li>
+              <li>Mujeres</li>
+              <li>Hombres</li>
+              <li>Relojes Lentes y Joyería</li>
+              <li>Perfumes y Cuidado Personal</li>
+              <li>Bebés y Niños</li>
+              <li>Juguetes</li>
+              <li>Motos y Automotriz</li>
+              <li>Deportes</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <input type="radio" name="accordion" id="sectionBlock3" className="Accordion__Input"/>
+          <label for="sectionBlock3" className="Accordion__Label">Ofertas</label>
+          <div className="Accordion__Content">
+            <ul>
+              <li>Mejores ofertas</li>
+              <li>Ofertas por categoría</li>
+              <li>Dinero Electrónico</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <input type="radio" name="accordion" id="sectionBlock3" className="Accordion__Input"/>
+          <label for="sectionBlock3" className="Accordion__Label">Exclusivos en línea</label>
+          <div className="Accordion__Content">
+            <ul>
+              <li>Exclusivos en línea</li>
+              <li>Exclusivos BanCoppel</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <input type="radio" name="accordion" id="sectionBlock3" className="Accordion__Input"/>
+          <label for="sectionBlock3" className="Accordion__Label">Servicios</label>
+          <div className="Accordion__Content">
+            <ul>
+              <li>Tiempo aire</li>
+              <li>Coppel Pay</li>
+              <li>Préstamo personal</li>
+              <li>Club de Protección</li>
+              <li>Coppel viajes</li>
+              <li>Cliente digital</li>
+              <li>Coppel Max</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
