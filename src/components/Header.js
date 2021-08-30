701cd317c7c66cf450c0d937c95f505da1c94ee8
jorge <georgellanque@gmail.com>
2021-08-29 20:15:35 -0400

    Add categories file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
+/* eslint-disable array-callback-return */
 import React, {useState} from 'react'
 import Logo from '../assets/images/logo.svg'
 import {HiMenuAlt1} from 'react-icons/hi'
 import {AiOutlineShoppingCart, AiOutlineArrowLeft, AiOutlineSearch} from 'react-icons/ai'
 import {IoLocationSharp} from 'react-icons/io5'
-import {IoIosArrowDown} from 'react-icons/io'
+import {IoIosArrowDown, IoIosArrowForward} from 'react-icons/io'
 import Navbar from './Navbar'
+import categories from '../utils/categories'
 import './styles/Header.scss'
 
 export default function Header() {
@@ -62,64 +64,28 @@ export default function Header() {
         </div>
         <div className="Header__Row3-Sideleft">
           <ul>
-          <li><a href="/">Departamentos<i><IoIosArrowDown/></i></a>
-            <ul>
-              <li><a href="/">Celulares</a>
-                <ul>
-                  <li><a href="/">Telcel</a></li>
-                  <li><a href="/">Movistar</a></li>
-                  <li><a href="/">Telefonía libre</a></li>
-                </ul>
-              </li>
-              <li><a href="/">Electrónica</a>
-                <ul>
-                  <li><a href="/">Telcel</a></li>
-                  <li><a href="/">Movistar</a></li>
-                  <li><a href="/">Telefonía libre</a></li>
-                </ul>
-              </li>
-              <li><a href="/">Consolas y Videojuegos</a></li>
-              <li><a href="/">Instrumentos Musicales</a></li>
-              <li><a href="/">Hogar y Muebles</a></li>
-              <li><a href="/">Línea Blanca y Electrodomésticos</a></li>
-              <li><a href="/">Zapatos</a></li>
-              <li><a href="/">Mujeres</a></li>
-              <li><a href="/">Hombres</a></li>
-            </ul>
-          </li>
-          <li><a href="/">Ofertas<i><IoIosArrowDown/></i></a>
-            <ul>
-              <li><a href="/">Mejores ofertas</a></li>
-              <li><a href="/">Ofertas por categoría</a></li>
-              <li><a href="/">Dinero Electrónico</a></li>
-            </ul>
-          </li>
-          <li><a href="/">Exclusivos en línea<i><IoIosArrowDown/></i></a>
-            <ul>
-              <li><a href="/">Exclusivos en línea</a></li>
-              <li><a href="/">Lo más nuevo</a></li>
-            </ul>
-          </li>
-          <li><a href="/">Servicios<i><IoIosArrowDown/></i></a>
-            <ul>
-              <li><a href="/">Tiempo aire</a></li>
-              <li><a href="/">Coppel Pay</a></li>
-              <li><a href="/">Préstamo personal</a></li>
-              <li><a href="/">Club de Protección</a></li>
-              <li><a href="/">Coppel viajes</a></li>
-              <li><a href="/">Cliente digital</a></li>
-              <li><a href="/">Coppel Max</a></li>
-              <li><a href="/">Sorteos y concursos</a></li>
-              <li><a href="/">Periódico Coppel</a></li>
-              <li><a href="/">Starhaus Primavera-Verano</a></li>
-              <li><a href="/">Revista Coppel</a></li>
-              <li><a href="/">Revista Celulares y Computación</a></li>
-              <li><a href="/">Starhaus Colecciones</a></li>
-              <li><a href="/">Mejora tu vida</a></li>
-              <li><a href="/">Periódicos y Revistas Coppel</a></li>
-              <li><a href="/">Políticas de servicios de remesas</a></li>
-            </ul>
-          </li>
+            {
+              categories.map(item => (
+                <li key={item.title}><a href="/">{item.title}<i><IoIosArrowDown/></i></a>
+                  <ul>
+                    {
+                      item.content.map(subitem => (
+                        subitem.title
+                        ? <li><a href="/">{subitem.title}<i><IoIosArrowForward/></i></a>
+                            <ul>
+                              {subitem.content.map(content => (
+                                <li><a href="/">{content}</a></li>
+                                ))
+                              }
+                            </ul>
+                          </li>
+                        : <li><a href="/">{subitem}</a></li>
+                      ))
+                    }
+                  </ul>
+                </li>
+              ))
+            }
           </ul>
         </div>
         <div className="Header__Row3-Sideright">
---
+++ src/utils/categories.js
@@ -0,0 +1,56 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  {
+    title: "Departamentos",
+    content: [
+      {
+        title: "Celulares",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Electrónica",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Consolas y Videojuegos",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Instrumentos Musicales",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Hogar y Muebles",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Línea Blanca y Electrodomésticos",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Zapatos",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Mujeres",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      },
+      {
+        title: "Hombres",
+        content: ["Telcel", "Movistar", "Telefonía libre"]
+      }
+    ]
+  },
+  {
+    title: "Ofertas",
+    content: ["Mejores ofertas", "Ofertas por categoría", "Dinero Electrónico"]
+  },
+  {
+    title: "Exclusivos en línea",
+    content: ["Exclusivos en línea", "Lo más nuevo"]
+  },
+  {
+    title: "Servicios",
+    content: ["Tiempo aire", "Coppel Pay", "Préstamo personal", "Club de Protección", "Coppel viajes", "Cliente digital", "Coppel Max", "Sorteos y concursos", "Periódico Coppel", "Starhaus Primavera-Verano", "Revista Coppel", "Revista Celulares y Computación", "Starhaus Colecciones", "Mejora tu vida", "Periódicos y Revistas Coppel", "Políticas de servicios de remesas"]
+  }
+]
