77874b9e0d00823ba7055cd247c1e319304d05bb
jorge <georgellanque@gmail.com>
2021-08-30 15:01:20 -0400

    Add custom properties

--- src/components/OffersAndNews.js
+++ src/components/OffersAndNews.js
@@ -15,15 +15,19 @@ export default function OffersAndNews() {
     "https://cdn2.coppel.com/images/emarketing/sliders/2021/s34_fds/desktop/s34_slider1_desktop_fds_gamer.jpg",
     "https://cdn2.coppel.com/images/emarketing/sliders/2021/s34_fds/desktop/s34_slider2_desktop_fds_colchones.jpg"
   ]
+  const offers = [
+    "https://cdn2.coppel.com/images/emarketing/homepage/2021/julio/s35/Home5_AGO21_S35_tablet_B2_calzadohombre.jpg",
+    "https://cdn2.coppel.com/images/emarketing/homepage/2021/julio/s35/Home5_AGO21_S35_tablet_B1_computo.jpg"
+  ]
 
   return (
     <div className="Container__OffersAndNews">
       <div className="Carousel">
-        <Carousel>
+        <Carousel autoPlay={true} infiniteLoop={true} showArrows={false} showThumbs={false} showStatus={false}>
           {
             sliders.map(sourceLink => (
               <div>
-                <img src={sourceLink} alt="sfwe"/>
+                <img src={sourceLink} alt={sourceLink}/>
               </div>
             ))
           }
@@ -31,10 +35,11 @@ export default function OffersAndNews() {
       </div>
       <div className="Offers">
         <div className="Offers-Thumbnail">
-
+          <img src={offers[0]} alt="asd"/>
         </div>
+        
         <div className="Offers-Thumbnail">
-
+          <img src={offers[1]} alt="asd"/>
         </div>
       </div>
     </div>
